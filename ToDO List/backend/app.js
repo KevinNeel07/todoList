f52113938e0e929bc7ac4f7d551de86b951f92f8
Uptake Infotech <uptakeinfotech@Uptakes-Mac-mini.local>
2022-09-27 18:03:59 +0530

    created completed task functionality

--- ToDO List/backend/app.js
+++ ToDO List/backend/app.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const fs = require('fs');
+const {parse} = require('querystring')
 
 let login = fs.readFileSync('../frontend/index.html', 'utf-8');
 let signUp = fs.readFileSync('../frontend/signUp/signUp.html', 'utf-8');
@@ -8,6 +9,17 @@ const server = http.createServer((req,res)=>{
     res.writeHead(200, {'Content-type': 'text/html'});
     if(req.url == '/'){
         res.end(login)
+        if(req.method === 'POST'){
+            let body;
+           req.on('data', chunk =>{
+            body = chunk.toString();
+            console.log(body);
+        })
+        req.on('end', ()=>{
+            let data = parse(body)
+            console.log(data.email);
+           })
+        }
     }
     if(req.url == '/signUp'){
         console.log('working');
